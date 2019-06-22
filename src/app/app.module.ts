0910e0f64112a7e741ee971c36e80ddba9c842bb
zk1u <joostderuijter@protonmail.com>
2019-06-22 22:08:27 +0200

    First version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutSectionComponent } from './pages/home/components/about-section/about-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutSectionComponent
   ],
   imports: [
     BrowserModule
