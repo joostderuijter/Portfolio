a17880bfc254372abe79f7fb8cc159d2bc592e88
zk1u <joostderuijter@protonmail.com>
2019-06-26 21:21:40 +0200

    Added 3 sample projects to carousel. Button clicking adds class to 'selected' project.

--- src/app/pages/home/components/projects-section/projects.component.ts
+++ src/app/pages/home/components/projects-section/projects.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Project} from '../../../../models/project';
+import {fromEvent, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit{
+  projects: Project[] = [
+    { id: 0, projectName: "Spotify infographic", projectContent: "I made an infographic of sorts" },
+    { id: 1, projectName: "Thuisbezorgd random food", projectContent: "I made an application which lets you order random food from thuisbezorgd" },
+    { id: 2, projectName: "This portfolio website", projectContent: "This website!" }
+  ];
+  selectedProject: Project;
+  index: number;
+  /*  nextButton = document.querySelector(".next");
+  nextClickStream = fromEvent(this.nextButton, 'click' );
+  previousButton = document.querySelector("btn previous");
+  previousClickStream = fromEvent(this.previousButton, 'click' );*/
 
-  constructor() { }
+  constructor() {
+    this.index = 0;
+  }
 
   ngOnInit() {
   }
 
+  onSelect(project: Project) {
+    this.selectedProject = project;
+  }
+
+  navigateCarousel(index: number) {
+    this.index += index;
+    if (this.index === -1) this.index = this.projects.length -1;
+    if (this.index % this.projects.length === 0) this.index = 0;
+  }
 }
